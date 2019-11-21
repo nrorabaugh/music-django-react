8cfd8aac18d61801af547547e83146ec454534b4
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-11-21 14:08:11 -0500

    Commit

--- tunr_project/tunr_app/models.py
+++ tunr_project/tunr_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100, default='n/a')
+    nationality = models.CharField(max_length=100, default='n/a')
+    photo_url = models.CharField(max_length=300, default='n/a')
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name='songs')
+    title = models.CharField(max_length=255, default='n/a')
+    album = models.CharField(max_length=255, default='n/a')
+    photo_url = models.CharField(max_length=300, default='n/a')
+
+    def __str__(self):
+        return self.title
